2617f5dffb00776748da073bdf4cd2693decd6ec
PASCAUD Antoine <38751850+AnPasco@users.noreply.github.com>
2018-12-30 18:18:38 +0100

    retour a la version précédente du code php

--- Ptut/index.php
+++ Ptut/index.php
@@ -1,54 +1,143 @@
-<?php
-$intervales = array( 6,13,
-                     3,14,
-                     1,15,
-                     2,15,
-                     2,15,
-                     2,15,
-                     1,15,
-                     1,15,
-                     0,15,
-                     1,15,
-                     1,15,
-                     4,15,
-                     5,15,
-                     5,13,
-                     5,12,
-                     5,12,
-                     6,12,
-                     9,10
-                  );
-$length = 18;
-$taille_carre = 47.33;
-$margin_x = 17.117637;
-$margin_y = 163.06255;
-
-?>
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Carte</title>
+  <title>Carte 47.5</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="map" id="map">
       <svg version="1.1" id="carte" viewBox="0 0 793.59998 1122.5601" sodipodi:docname="Carte_Lim_mailles-page-001.svg"
        inkscape:version="0.92.3 (2405546, 2018-03-11)">
-      <image xlink:href="./Carte_Lim_mailles-page-001.svg" id="Carte_Limmailles" />
+      <image xlink:href="./Carte_Lim_mailles-page-001.svg" id="Carte_Lim_mailles" />
 
       <?php
-         for($i = 0; $i<$length; $i++){
-            $debut = $intervales[$i2];
-            $fin =  $intervales[$i2+1];
-
-            for($colonne = $debut; $colonne <= $fin ; $colonne++){
-               ?>
-               <a id="carre<?php echo $i?>- <?php echo $colonne ?>" xlink:href="http://www.google.com/" xlink:title="carre_<?php echo $i?>-<?php echo $colonne ?>">
-                  <path d="m <?php echo $margin_x + ($colonne * $taille_carre) ?>,<?php echo $margin_y + $i * $taille_carre ?> -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
-               </a>
-               <?php
-            }
+         define('taille_carre', '47.33');
+         for($colonne = 1; $colonne < 9 ; $colonne++){
+            $ligne = 253.52561; ?>
+            <a id="carre_1- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_1-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,163.06255 -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 13 ; $colonne++){
+            $ligne = 111.50393; ?>
+            <a id="carre_2- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_2-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,210.69656 -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 16 ; $colonne++){
+            $ligne = 17.117637; ?>
+            <a id="carre_3- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_3-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,257.7846 -0.14864,44.74187 45.039153,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 15 ; $colonne++){
+            $ligne = 64.67331; ?>
+            <a id="carre_4- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_4-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,305.16546 -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 15 ; $colonne++){
+            $ligne = 64.67331; ?>
+            <a id="carre_5- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_5-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,352.43428 -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 15 ; $colonne++){
+            $ligne = 64.67331; ?>
+            <a id="carre_6- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_6-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,399.70309 -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 16 ; $colonne++){
+            $ligne = 16.809915; ?>
+            <a id="carre_7- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_7-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,446.82326 -0.14864,44.74187 45.039155,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 16 ; $colonne++){
+            $ligne = 16.809915; ?>
+            <a id="carre_8- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_8-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,494.24073 -0.14864,44.74187 45.03915,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 17 ; $colonne++){
+            $ligne = -30.607551; ?>
+            <a id="carre_9- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_9-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,541.65819 -0.14864,44.74186 45.03916,0.29728 -0.14864,-45.18779 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 16 ; $colonne++){
+            $ligne = 16.809915; ?>
+            <a id="carre_10- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_10-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,589.39792 -0.14864,44.74187 45.039162,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 16 ; $colonne++){
+            $ligne = 16.809915; ?>
+            <a id="carre_11- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_11-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,636.69618 -0.14864,44.74187 45.039156,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 13 ; $colonne++){
+            $ligne = 159.1175; ?>
+            <a id="carre_12- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_12-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,684.09955 -0.14864,44.74187 45.03916,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 12 ; $colonne++){
+            $ligne = 206.41576; ?>
+            <a id="carre_13- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_13-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,731.18759 -0.14864,44.74187 45.03916,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 10 ; $colonne++){
+            $ligne = 206.41576; ?>
+            <a id="carre_14- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_14-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,778.38073 -0.14864,44.74187 45.03916,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 9 ; $colonne++){
+            $ligne = 206.41576; ?>
+            <a id="carre_15- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_15-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,825.25856 -0.14864,44.74187 45.03916,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 9 ; $colonne++){
+            $ligne = 206.41576; ?>
+            <a id="carre_16- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_16-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,872.66192 -0.14864,44.74187 45.03916,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 8 ; $colonne++){
+            $ligne = 254.02934; ?>
+            <a id="carre_17- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_17-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,920.06528 -0.14864,44.74187 45.03916,0.29728 -0.14864,-45.1878 z" />
+            </a>
+            <?php
+         }
+         for($colonne = 1; $colonne < 3 ; $colonne++){
+            $ligne = 395.29346; ?>
+            <a id="carre_18- <?php echo $colonne ?>" xlink:href="http://www.google.com" xlink:title="carre_18-<?php echo $colonne ?>">
+               <path d="m <?php echo $ligne + ($colonne * taille_carre) ?>,967.57376 -0.14864,44.74194 45.03916,0.2972 -0.14864,-45.18779 z" />
+            </a>
+            <?php
          }
        ?>
     </svg>
